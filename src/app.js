3a751923fa48485854bf247ff67a3371a5b5b1ff
Roderick Fung <roderickfung@users.noreply.github.com>
2018-11-09 16:30:06 -0800

    basic SSR working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from 'components/pages/home';
+import Home from './components/pages/home';
 
 const App = () => {
   return <Home />;
--- webpack.server.js
+++ webpack.server.js
@@ -15,15 +15,19 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                'react',
+                'stage-0',
+                ['env', { targets: { browsers: ['last 2 versions'] } }]
+              ]
+            }
+          }
+        ]
       }
     ]
   }
